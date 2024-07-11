0387a8e1143998a07166a0ff803a853eb4c9d78e
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2024-07-11 15:30:49 -0300

    Sign in page styled

--- Front-end/Worldlet/src/pages/Sign In/Sign_in.jsx
+++ Front-end/Worldlet/src/pages/Sign In/Sign_in.jsx
@@ -3,16 +3,24 @@ import Header from "../../components/Header/Header"
 import "./Sign_in.css"
 
 export default function Sign_in(){
-    return(
-        <div className='container'>
-            <Header/>
-            <div className="content">
-                <h2>Open your worldwide wallet today</h2>
 
-                <h4>Worldlet is the first multi-currency digital wallet application available in the world.
-                    What are you waiting for? Start saving money in any currency you want and use any currency for specific payments.
-                </h4>
-                <Link className="start-now" >Start now</Link>
+    const login = ()=>{
+
+    }
+
+    return(
+        <div className='login-container'>
+            <Header/>
+            <div className="login-content">
+                <form className="login-form" onSubmit={login}>
+                    <h3 id="login-h3">Login</h3>
+                    <label>E-mail:</label>
+                    <input type="email" placeholder="Enter email"/>
+                    <label>Password:</label>
+                    <input type="password" placeholder="Enter password"/>
+                    <input type="submit" value="Login"/>
+                    <p>Don't have an account? <Link to={"/Sign-up"}>Signup</Link></p>
+                </form>
             </div>
         </div>
     )
